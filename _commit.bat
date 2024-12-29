6e99ab9ef1534ed011c5f0b64b4561b3dae53178
IvanS <test@gmail.com>
2024-12-29 19:46:02 +0500

    v1.2.7 commit

--- _commit.bat
+++ _commit.bat
@@ -1,4 +1,4 @@
-set VTAG=v1.2.6
+set VTAG=v1.2.7
 
 git add -A
 git commit -m "%VTAG% commit"
--- v1.go
+++ v1.go
@@ -5,7 +5,7 @@ func Vad(a int, b int) (c int) {
 
   var ab int
   ab = a + b + 1118
-  ab = 555
+  ab = 5558
 
   return ab
 }
